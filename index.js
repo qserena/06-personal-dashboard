a33b169b15707159a54b57e197bd3f059e1d6544
Tom Serenander <tom@serenander.se>
2023-10-27 15:59:49 +0200

    Ready for Chrome extension

--- index.js
+++ index.js
@@ -40,6 +40,42 @@ function getCurrentTime() {
 	)
 }
 
+function getWeather() {
+	navigator.geolocation.getCurrentPosition(
+		(position) => {
+			fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&
+            lon=${position.coords.longitude}&units=metric`)
+				.then((res) => {
+					if (!res.ok) {
+						throw Error('Weather data not available')
+					}
+					return res.json()
+				})
+				.then((data) => {
+					console.log(data)
+					const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+					document.getElementById('weather').innerHTML = `
+						<img src="${iconUrl}" />
+						<p>${Math.round(data.main.temp)}º</p>
+						<p>${Math.round(data.name)}</p>
+					`
+				})
+				.catch((err) => console.error(err))
+		},
+		(err) => {
+			console.log(err)
+			document.getElementById('weather').innerHTML = `
+						<img class="icon" src="./img/sun_icon.png" />
+						<p>32º</p>
+						<p>Stockholm</p>
+						`
+		},
+		{ timeout: 10000 }
+	)
+}
+
 init()
+
+getWeather()
 
 setInterval(getCurrentTime, 1000)
